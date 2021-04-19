22caa8b88813544e6381b20abd83b3c2bbac3d5e
Torjus Kallekleiv <torjus.kallekleiv@gmail.com>
2021-04-19 15:30:35 +0200

    Alle oppgaver løst.

--- ChordDHTPeer-Project3-Oppgave/src/no/hvl/dat110/util/FileManager.java
+++ ChordDHTPeer-Project3-Oppgave/src/no/hvl/dat110/util/FileManager.java
@@ -1,6 +1,5 @@
 package no.hvl.dat110.util;
 
-
 /**
  * @author tdoy
  * dat110 - project 3
@@ -22,248 +21,311 @@ import no.hvl.dat110.middleware.Message;
 import no.hvl.dat110.rpc.interfaces.NodeInterface;
 import no.hvl.dat110.util.Hash;
 
-public class FileManager {
-	
-	private BigInteger[] replicafiles;							// array stores replicated files for distribution to matching nodes
-	private int numReplicas;									// let's assume each node manages nfiles (5 for now) - can be changed from the constructor
+public class FileManager
+{
+
+	private BigInteger[] replicafiles; // array stores replicated files for distribution to matching nodes
+	private int numReplicas; // let's assume each node manages nfiles (5 for now) - can be changed from the
+								// constructor
 	private NodeInterface chordnode;
-	private String filepath; 									// absolute filepath
-	private String filename;									// only filename without path and extension
+	private String filepath; // absolute filepath
+	private String filename; // only filename without path and extension
 	private BigInteger hash;
 	private byte[] bytesOfFile;
 	private String sizeOfByte;
-	
+
 	private Set<Message> activeNodesforFile = null;
-	
-	public FileManager(NodeInterface chordnode) throws RemoteException {
+
+	public FileManager(NodeInterface chordnode) throws RemoteException
+	{
 		this.chordnode = chordnode;
 	}
-	
-	public FileManager(NodeInterface chordnode, int N) throws RemoteException {
+
+	public FileManager(NodeInterface chordnode, int N) throws RemoteException
+	{
 		this.numReplicas = N;
 		replicafiles = new BigInteger[N];
 		this.chordnode = chordnode;
 	}
-	
-	public FileManager(NodeInterface chordnode, String filepath, int N) throws RemoteException {
+
+	public FileManager(NodeInterface chordnode, String filepath, int N) throws RemoteException
+	{
 		this.filepath = filepath;
 		this.numReplicas = N;
 		replicafiles = new BigInteger[N];
 		this.chordnode = chordnode;
 	}
-	
-	public void createReplicaFiles() {
-	 	
-		// implement
-		
-		// set a loop where size = numReplicas
-		
-		// replicate by adding the index to filename
-		
-		// hash the replica
-		
-		// store the hash in the replicafiles array.
 
+	public void createReplicaFiles()
+	{
+
+		// implement
+
+		// set a loop where size = numReplicas
+		for (int i = 0; i < numReplicas; i++)
+		{
+			// replicate by adding the index to filename
+			String temp = filename + i;
+
+			// hash the replica
+			hash = Hash.hashOf(temp);
+
+			// store the hash in the replicafiles array.
+			replicafiles[i] = hash;
+
+		}
 	}
-	
-    /**
-     * 
-     * @param bytesOfFile
-     * @throws RemoteException 
-     */
-    public int distributeReplicastoPeers() throws RemoteException {
-    	int counter = 0;
-    	
-    	// Task1: Given a filename, make replicas and distribute them to all active peers such that: pred < replica <= peer
-    	
-    	// Task2: assign a replica as the primary for this file. Hint, see the slide (project 3) on Canvas
-    	
-    	// create replicas of the filename
-    	
+
+	/**
+	 * 
+	 * @param bytesOfFile
+	 * @throws RemoteException
+	 */
+	public int distributeReplicastoPeers() throws RemoteException
+	{
+		int counter = 0;
+
+		// Task1: Given a filename, make replicas and distribute them to all active
+		// peers such that: pred < replica <= peer
+		createReplicaFiles();
+
+		// Task2: assign a replica as the primary for this file. Hint, see the slide
+		// (project 3) on Canvas
+		Random rnd = new Random();
+		int index = rnd.nextInt(Util.numReplicas - 1);
+
+		// create replicas of the filename
+
 		// iterate over the replicas
-    	
-    	// for each replica, find its successor by performing findSuccessor(replica)
-    	
-    	// call the addKey on the successor and add the replica
-    	
-    	// call the saveFileContent() on the successor
-    	
-    	// increment counter
-    	
-    		
+		for (BigInteger key : replicafiles)
+		{
+
+			// for each replica, find its successor by performing findSuccessor(replica)
+			NodeInterface succ = chordnode.findSuccessor(key);
+
+			// call the addKey on the successor and add the replica
+			succ.addKey(key);
+
+			// call the saveFileContent() on the successor
+			succ.saveFileContent(filename, key, bytesOfFile, counter == index);
+
+			// increment counter
+			counter++;
+		}
 		return counter;
-    }
-	
+	}
+
 	/**
 	 * 
 	 * @param filename
 	 * @return list of active nodes having the replicas of this file
-	 * @throws RemoteException 
+	 * @throws RemoteException
 	 */
-	public Set<Message> requestActiveNodesForFile(String filename) throws RemoteException {
-		
+	public Set<Message> requestActiveNodesForFile(String filename) throws RemoteException
+	{
+
 		this.filename = filename;
 		Set<Message> succinfo = new HashSet<Message>();
+
 		// Task: Given a filename, find all the peers that hold a copy of this file
-		
+
 		// generate the N replicas from the filename by calling createReplicaFiles()
-		
+		createReplicaFiles();
+
 		// it means, iterate over the replicas of the file
-		
-		// for each replica, do findSuccessor(replica) that returns successor s.
-		
-		// get the metadata (Message) of the replica from the successor, s (i.e. active peer) of the file
-		
-		// save the metadata in the set succinfo.
-		
+		for (BigInteger key : replicafiles)
+		{
+			// for each replica, do findSuccessor(replica) that returns successor s.
+			NodeInterface succ = chordnode.findSuccessor(key);
+			
+			// get the metadata (Message) of the replica from the successor, s (i.e. active peer) of the file
+			Message message = succ.getFilesMetadata().get(key);
+			
+			// save the metadata in the set succinfo.
+			succinfo.add(message);
+		}
+	
+
 		this.activeNodesforFile = succinfo;
-		
+
 		return succinfo;
 	}
-	
+
 	/**
 	 * Find the primary server - Remote-Write Protocol
-	 * @return 
+	 * 
+	 * @return
 	 */
-	public NodeInterface findPrimaryOfItem() {
+	public NodeInterface findPrimaryOfItem()
+	{
 
-		// Task: Given all the active peers of a file (activeNodesforFile()), find which is holding the primary copy
-		
+		// Task: Given all the active peers of a file (activeNodesforFile()), find which
+		// is holding the primary copy
+
 		// iterate over the activeNodesforFile
-		
-		// for each active peer (saved as Message)
-		
-		// use the primaryServer boolean variable contained in the Message class to check if it is the primary or not
-		
-		// return the primary
-		
-		return null; 
+		for (Message message : activeNodesforFile)
+		{
+			// for each active peer (saved as Message)
+
+			// use the primaryServer boolean variable contained in the Message class to check if it is the primary or not
+			if (message.isPrimaryServer())
+			{
+				// return the primary
+				return Util.getProcessStub(message.getNodeIP(), message.getPort());
+			}
+
+		}
+		return null;
 	}
-	
-    /**
-     * Read the content of a file and return the bytes
-     * @throws IOException 
-     * @throws NoSuchAlgorithmException 
-     */
-    public void readFile() throws IOException, NoSuchAlgorithmException {
-    	
-    	File f = new File(filepath);
-    	
-    	byte[] bytesOfFile = new byte[(int) f.length()];
-    	
+
+	/**
+	 * Read the content of a file and return the bytes
+	 * 
+	 * @throws IOException
+	 * @throws NoSuchAlgorithmException
+	 */
+	public void readFile() throws IOException, NoSuchAlgorithmException
+	{
+
+		File f = new File(filepath);
+
+		byte[] bytesOfFile = new byte[(int) f.length()];
+
 		FileInputStream fis = new FileInputStream(f);
-        
-        fis.read(bytesOfFile);
+
+		fis.read(bytesOfFile);
 		fis.close();
-		
-		//set the values
-		filename = f.getName().replace(".txt", "");		
+
+		// set the values
+		filename = f.getName().replace(".txt", "");
 		hash = Hash.hashOf(filename);
 		this.bytesOfFile = bytesOfFile;
-		double size = (double) bytesOfFile.length/1000;
+		double size = (double) bytesOfFile.length / 1000;
 		NumberFormat nf = new DecimalFormat();
 		nf.setMaximumFractionDigits(3);
 		sizeOfByte = nf.format(size);
-		
-		System.out.println("filename="+filename+" size="+sizeOfByte);
-    	
-    }
-    
-    public void printActivePeers() {
-    	
-    	activeNodesforFile.forEach(m -> {
-    		String peer = m.getNodeIP();
-    		String id = m.getNodeID().toString();
-    		String name = m.getNameOfFile();
-    		String hash = m.getHashOfFile().toString();
-    		int size = m.getBytesOfFile().length;
-    		
-    		System.out.println(peer+": ID = "+id+" | filename = "+name+" | HashOfFile = "+hash+" | size ="+size);
-    		
-    	});
-    }
+
+		System.out.println("filename=" + filename + " size=" + sizeOfByte);
+
+	}
+
+	public void printActivePeers()
+	{
+
+		activeNodesforFile.forEach(m -> {
+			String peer = m.getNodeIP();
+			String id = m.getNodeID().toString();
+			String name = m.getNameOfFile();
+			String hash = m.getHashOfFile().toString();
+			int size = m.getBytesOfFile().length;
+
+			System.out.println(
+					peer + ": ID = " + id + " | filename = " + name + " | HashOfFile = " + hash + " | size =" + size);
+
+		});
+	}
 
 	/**
 	 * @return the numReplicas
 	 */
-	public int getNumReplicas() {
+	public int getNumReplicas()
+	{
 		return numReplicas;
 	}
-	
+
 	/**
 	 * @return the filename
 	 */
-	public String getFilename() {
+	public String getFilename()
+	{
 		return filename;
 	}
+
 	/**
 	 * @param filename the filename to set
 	 */
-	public void setFilename(String filename) {
+	public void setFilename(String filename)
+	{
 		this.filename = filename;
 	}
+
 	/**
 	 * @return the hash
 	 */
-	public BigInteger getHash() {
+	public BigInteger getHash()
+	{
 		return hash;
 	}
+
 	/**
 	 * @param hash the hash to set
 	 */
-	public void setHash(BigInteger hash) {
+	public void setHash(BigInteger hash)
+	{
 		this.hash = hash;
 	}
+
 	/**
 	 * @return the bytesOfFile
-	 */ 
-	public byte[] getBytesOfFile() {
+	 */
+	public byte[] getBytesOfFile()
+	{
 		return bytesOfFile;
 	}
+
 	/**
 	 * @param bytesOfFile the bytesOfFile to set
 	 */
-	public void setBytesOfFile(byte[] bytesOfFile) {
+	public void setBytesOfFile(byte[] bytesOfFile)
+	{
 		this.bytesOfFile = bytesOfFile;
 	}
+
 	/**
 	 * @return the size
 	 */
-	public String getSizeOfByte() {
+	public String getSizeOfByte()
+	{
 		return sizeOfByte;
 	}
+
 	/**
 	 * @param size the size to set
 	 */
-	public void setSizeOfByte(String sizeOfByte) {
+	public void setSizeOfByte(String sizeOfByte)
+	{
 		this.sizeOfByte = sizeOfByte;
 	}
 
 	/**
 	 * @return the chordnode
 	 */
-	public NodeInterface getChordnode() {
+	public NodeInterface getChordnode()
+	{
 		return chordnode;
 	}
 
 	/**
 	 * @return the activeNodesforFile
 	 */
-	public Set<Message> getActiveNodesforFile() {
+	public Set<Message> getActiveNodesforFile()
+	{
 		return activeNodesforFile;
 	}
 
 	/**
 	 * @return the replicafiles
 	 */
-	public BigInteger[] getReplicafiles() {
+	public BigInteger[] getReplicafiles()
+	{
 		return replicafiles;
 	}
 
 	/**
 	 * @param filepath the filepath to set
 	 */
-	public void setFilepath(String filepath) {
+	public void setFilepath(String filepath)
+	{
 		this.filepath = filepath;
 	}
 }
--- ChordDHTPeer-Project3-Oppgave/src/no/hvl/dat110/util/Hash.java
+++ ChordDHTPeer-Project3-Oppgave/src/no/hvl/dat110/util/Hash.java
@@ -1,66 +1,92 @@
 package no.hvl.dat110.util;
 
-/**
- * project 3
- * @author tdoy
- *
- */
-
-import java.io.UnsupportedEncodingException;
 import java.math.BigInteger;
-import java.net.UnknownHostException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
-public class Hash { 
-	
-	private static BigInteger hashint; 
-	
-	public static BigInteger hashOf(String entity) {		
-		
+public class Hash
+{
+
+	private static BigInteger hashint;
+
+	public static BigInteger hashOf(String entity)
+	{
 		// Task: Hash a given string using MD5 and return the result as a BigInteger.
-		
-		// we use MD5 with 128 bits digest
-		
-		// compute the hash of the input 'entity'
-		
-		// convert the hash into hex format
-		
-		// convert the hex into BigInteger
-		
+
+		try
+		{
+			// we use MD5 with 128 bits digest
+			MessageDigest md = MessageDigest.getInstance("MD5");
+			// compute the hash of the input 'entity'
+			md.update(entity.getBytes());
+			byte[] digest = md.digest();
+
+			// convert the hash into hex format
+			String hexString = toHex(digest);
+
+			// convert the hex into BigInteger
+			hashint = new BigInteger(hexString, 16);
+		}
+		catch (NoSuchAlgorithmException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 		// return the BigInteger
-		
 		return hashint;
 	}
-	
-	public static BigInteger addressSize() {
-		
+
+	public static BigInteger addressSize()
+	{
 		// Task: compute the address size of MD5
-		
+
 		// get the digest length
+		int digestlen = 0;
+		try
+		{
+			digestlen = MessageDigest.getInstance("MD5").getDigestLength();
+			
+		} catch (NoSuchAlgorithmException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		
 		// compute the number of bits = digest length * 8
+		int numBits = digestlen * 8;
 		
-		// compute the address size = 2 ^ number of bits
+		// compute the address size = 2 ^ number of bit
+		BigInteger adSize = BigInteger.TWO.pow(numBits);
 		
 		// return the address size
-		
-		return null;
+		return adSize;
 	}
-	
-	public static int bitSize() {
-		
+
+	public static int bitSize()
+	{
 		int digestlen = 0;
 		
 		// find the digest length
+		try
+		{
+			digestlen = MessageDigest.getInstance("MD5").getDigestLength();
+		} catch (NoSuchAlgorithmException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
-		return digestlen*8;
+		return digestlen * 8;
 	}
-	
-	public static String toHex(byte[] digest) {
+
+	public static String toHex(byte[] digest)
+	{
 		StringBuilder strbuilder = new StringBuilder();
-		for(byte b : digest) {
-			strbuilder.append(String.format("%02x", b&0xff));
+		for (byte b : digest)
+		{
+			strbuilder.append(String.format("%02x", b & 0xff));
 		}
 		return strbuilder.toString();
 	}
